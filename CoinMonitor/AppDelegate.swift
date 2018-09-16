73a1a9834cdb6398bbae93036ce40c3ef6ed5acf
jiangliao <jiangliao@u51.com>
2018-09-16 20:48:34 +0800

    Initial Commit

---
+++ CoinMonitor/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CoinMonitor
+//
+//  Created by 姜辽 on 2018/9/16.
+//  Copyright © 2018 姜辽. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
